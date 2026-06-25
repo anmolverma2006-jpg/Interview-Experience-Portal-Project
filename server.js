cb8b514ce5849ce676b950f00d002dc7fc197789
Anmol Verma <anmolverma2006@example.com>
2026-06-25 15:55:26 +0530

    log requests

--- server.js
+++ server.js
@@ -11,6 +11,11 @@ const PORT = process.env.PORT || 4000;
 
 console.log("PORT =", process.env.PORT);
 
+app.use((req, res, next) => {
+    console.log(`${req.method} ${req.url}`);
+    next();
+});
+
 app.get("/", (req, res) => {
     res.send("Backend is working!");
 });
